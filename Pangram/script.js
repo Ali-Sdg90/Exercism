47bea3bb96dfa1b1e9bbf1683aad36b82dca12ec
Ali Sadeghi <ali909392@gmail.com>
2023-05-22 13:49:03 +0330

    Small chagnes in leap and pangram

--- Pangram/script.js
+++ Pangram/script.js
@@ -3,12 +3,12 @@ const isPangram = (sentence) => {
     sentence = sentence.toLowerCase();
     for (let i = 0; i < alphabet.length; i++) {
         for (let j = 0; j < sentence.length; j++) {
-            if (sentence.charAt(j) == alphabet.charAt(i)) {
+            if (sentence.charAt(j) === alphabet.charAt(i)) {
                 alphabet = alphabet.replace(alphabet.charAt(i), "-");
             }
         }
     }
-    if (alphabet == "--------------------------") {
+    if (alphabet === "--------------------------") {
         return true;
     } else {
         return false;
