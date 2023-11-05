fc3804b38f825ce81ec4cb48c95af0c6a2a4ea00
Ali Sadeghi <ali909392@gmail.com>
2023-11-05 13:53:47 +0330

    Finish Grade School / Add Grade School to HUB

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,3 @@
 document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault();
 });
-
--- Grade School/script.js
+++ Grade School/script.js
@@ -28,7 +28,7 @@ class GradeSchool {
         for (let key in this.students) {
             for (let value = 0; value < this.students[key].length; value++) {
                 if (this.students[key][value] === name) {
-                    console.log(this.students[key][value] + " got deleted!");
+                    // console.log(this.students[key][value] + " got deleted!");
                     this.students[key].splice(value, 1);
                     return;
                 }
@@ -55,23 +55,140 @@ class GradeSchool {
     }
 }
 
+let expectedDb = "";
+let school = {};
+
+// console.log(JSON.stringify(school.grade(5)));
+// console.log(JSON.stringify(school.roster()));
+// console.log(JSON.stringify(expectedDb));
+
+// -----------------------------------
 school = new GradeSchool();
 
-// school.add("Aimee", 2);
-// school.grade(2).push("Oops.");
+school.add("Blair", 2);
+school.add("James", 2);
+school.add("Paul", 2);
+
+expectedDb = { 2: ["Blair", "James", "Paul"] };
+console.log(
+    "1.",
+    JSON.stringify(school.roster()) === JSON.stringify(expectedDb)
+);
+
+// -----------------------------------
+
+school = new GradeSchool();
+
+school.add("Chelsea", 3);
+school.add("Logan", 7);
+
+expectedDb = { 3: ["Chelsea"], 7: ["Logan"] };
+console.log(
+    "2.",
+    JSON.stringify(school.roster()) === JSON.stringify(expectedDb)
+);
+
+// -----------------------------------
+
+school = new GradeSchool();
+
+school.add("Franklin", 5);
+school.add("Bradley", 5);
+school.add("Jeff", 1);
+
+expectedDb = ["Bradley", "Franklin"];
+console.log(
+    "3.",
+    JSON.stringify(school.grade(5)) === JSON.stringify(expectedDb)
+);
+
+// -----------------------------------
+
+school = new GradeSchool();
+
+school.add("Jennifer", 4);
+school.add("Kareem", 6);
+school.add("Christopher", 4);
+school.add("Kyle", 3);
+
+expectedDb = {
+    3: ["Kyle"],
+    4: ["Christopher", "Jennifer"],
+    6: ["Kareem"],
+};
+console.log(
+    "4.",
+    JSON.stringify(school.roster()) === JSON.stringify(expectedDb)
+);
+
+// -----------------------------------
+
+school = new GradeSchool();
 
 school.add("Aimee", 2);
 const roster = school.roster();
 roster[2].push("Oops.");
 
-// console.log(JSON.stringify(school.grade(5)));
-console.log(JSON.stringify(school.roster()));
+expectedDb = { 2: ["Aimee"] };
+console.log(
+    "5.",
+    JSON.stringify(school.roster()) === JSON.stringify(expectedDb)
+);
 
-// const input = document.getElementById("input");
-// const btn = document
-//     .getElementById("btn")
-//     .addEventListener("click", function () {
-//         document.getElementById("output").textContent =
-//             "=> " + isLeap(input.value);
-//         console.log(isLeap(input.value));
-//     });
+// -----------------------------------
+
+school = new GradeSchool();
+
+school.add("Aimee", 2);
+school.grade(2).push("Oops.");
+
+expectedDb = { 2: ["Aimee"] };
+console.log(
+    "6.",
+    JSON.stringify(school.roster()) === JSON.stringify(expectedDb)
+);
+
+// -----------------------------------
+
+school = new GradeSchool();
+
+school.add("Aimee", 2);
+school.add("Aimee", 1);
+
+expectedDb = [];
+console.log(
+    "7.",
+    JSON.stringify(school.grade(2)) === JSON.stringify(expectedDb)
+);
+
+// -----------------------------------
+
+school = new GradeSchool();
+
+let inputCodes = `
+school.add("Aimee", 2);
+school.grade(2).push("Oops.");
+school.add("Aimee", 1);
+school.add("Jennifer", 4);
+school.add("Kareem", 6);
+school.add("Christopher", 4);
+school.add("Kyle", 2);
+`;
+
+const input = document.getElementById("input-text");
+const output = document.getElementById("output");
+
+input.value = inputCodes;
+
+eval(inputCodes);
+
+console.log(JSON.stringify(school.roster(), null, 2));
+output.value = JSON.stringify(school.roster(), null, 2);
+
+document.getElementById("form").addEventListener("submit", () => {
+    school = new GradeSchool();
+    eval(input.value);
+
+    console.log(JSON.stringify(school.roster(), null, 2));
+    output.value = JSON.stringify(school.roster(), null, 2);
+});
