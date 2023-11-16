0dc76d89405811f62a91ce8afd908d1fc15577fa
Ali Sadeghi <ali909392@gmail.com>
2023-11-16 22:37:39 +0330

    Add Simple Cipher codes / Simple Cipher showcase is SUPER RESPONSIVE!

---
+++ Simple Cipher/Just answer/script.js
@@ -0,0 +1,76 @@
+class Cipher {
+    constructor(key = "") {
+        this.keyValue = key;
+        this.encodeValue = "";
+        this.decodeValue = "";
+    }
+
+    encode(input) {
+        if (!this.keyValue) {
+            this.noKeyGiven(input.length);
+        } else if (this.keyValue.length < input.length) {
+            this.loopGivenKey(input.length);
+        }
+
+        this.encodeValue = "";
+        for (let i = 0; i < input.length; i++) {
+            let charCode =
+                input.charAt(i).charCodeAt(0) +
+                (this.key.charAt(i).charCodeAt(0) - 97);
+
+            if (charCode > 122) {
+                charCode -= 26;
+            }
+
+            this.encodeValue += String.fromCharCode(charCode);
+        }
+
+        return this.encodeValue;
+    }
+
+    decode(input) {
+        if (!this.keyValue) {
+            this.noKeyGiven(input.length);
+        } else if (this.keyValue.length < input.length) {
+            this.loopGivenKey(input.length);
+        }
+
+        this.decodeValue = "";
+        for (let i = 0; i < input.length; i++) {
+            let charCode =
+                input.charAt(i).charCodeAt(0) -
+                (this.key.charAt(i).charCodeAt(0) - 97);
+
+            if (charCode < 97) {
+                charCode += 26;
+            }
+
+            this.decodeValue += String.fromCharCode(charCode);
+        }
+
+        return this.decodeValue;
+    }
+
+    noKeyGiven(inputLength) {
+        console.log("noKeyGiven");
+
+        for (let i = 0; i < inputLength; i++) {
+            this.keyValue += String.fromCharCode(
+                Math.trunc(Math.random() * 26) + 97
+            );
+        }
+    }
+
+    loopGivenKey(inputLength) {
+        console.log("loopGivenKey");
+
+        const loopNumber = inputLength / this.keyValue.length;
+        for (let i = 0; i < loopNumber; i++) {
+            this.keyValue += this.keyValue;
+        }
+    }
+
+    get key() {
+        return this.keyValue;
+    }
+}
---
+++ Simple Cipher/script.js
@@ -0,0 +1,146 @@
+class Cipher {
+    constructor(key = "") {
+        this.keyValue = key;
+        this.encodeValue = "";
+        this.decodeValue = "";
+    }
+
+    encode(input) {
+        if (!this.keyValue) {
+            this.noKeyGiven(input.length);
+        } else if (this.keyValue.length < input.length) {
+            this.loopGivenKey(input.length);
+        }
+
+        this.encodeValue = "";
+        for (let i = 0; i < input.length; i++) {
+            let charCode =
+                input.charAt(i).charCodeAt(0) +
+                (this.key.charAt(i).charCodeAt(0) - 97);
+
+            if (charCode > 122) {
+                charCode -= 26;
+            }
+
+            this.encodeValue += String.fromCharCode(charCode);
+        }
+
+        return this.encodeValue;
+    }
+
+    decode(input) {
+        if (!this.keyValue) {
+            this.noKeyGiven(input.length);
+        } else if (this.keyValue.length < input.length) {
+            this.loopGivenKey(input.length);
+        }
+
+        this.decodeValue = "";
+        for (let i = 0; i < input.length; i++) {
+            let charCode =
+                input.charAt(i).charCodeAt(0) -
+                (this.key.charAt(i).charCodeAt(0) - 97);
+
+            if (charCode < 97) {
+                charCode += 26;
+            }
+
+            this.decodeValue += String.fromCharCode(charCode);
+        }
+
+        return this.decodeValue;
+    }
+
+    noKeyGiven(inputLength) {
+        // console.log("noKeyGiven");
+
+        for (let i = 0; i < inputLength; i++) {
+            this.keyValue += String.fromCharCode(
+                Math.trunc(Math.random() * 26) + 97
+            );
+        }
+    }
+
+    loopGivenKey(inputLength) {
+        // console.log("loopGivenKey");
+
+        const loopNumber = inputLength / this.keyValue.length;
+        for (let i = 0; i < loopNumber; i++) {
+            this.keyValue += this.keyValue;
+        }
+    }
+
+    get key() {
+        return this.keyValue;
+    }
+}
+
+// Tests:
+
+const cipher3 = new Cipher();
+console.log(cipher3.encode("aaaaaaaaaa") === cipher3.key.substring(0, 10));
+console.log(cipher3.decode(cipher3.key.substring(0, 10)) === "aaaaaaaaaa");
+const plaintext2 = "abcdefghij";
+console.log(cipher3.decode(cipher3.encode(plaintext2)) === plaintext2);
+
+const key2 = "abcdefghij";
+const cipher4 = new Cipher(key2);
+console.log(cipher4.encode("aaaaaaaaaa") === "abcdefghij");
+console.log(cipher4.decode("abcdefghij") === "aaaaaaaaaa");
+console.log(cipher4.decode(cipher4.encode("abcdefghij")) === "abcdefghij");
+console.log(
+    new Cipher("iamapandabear").encode("iamapandabear") === "qayaeaagaciai"
+);
+console.log(cipher4.encode("zzzzzzzzzz") === "zabcdefghi");
+console.log(cipher4.decode("zabcdefghi") === "zzzzzzzzzz");
+console.log(new Cipher("abc").encode("iamapandabear") === "iboaqcnecbfcr");
+console.log(new Cipher("abc").decode("iboaqcnecbfcr") === "iamapandabear");
+
+// Showcase:
+
+const input = document.getElementById("input-text");
+const output = document.getElementById("output");
+const submitBtn = document.getElementById("btn");
+
+let ans = [];
+
+input.value = `
+    const cipher1 = new Cipher();
+    ans[0] = cipher1.encode("aaaaaaaaaa") === cipher1.key.substring(0, 10);
+    ans[1] = cipher1.decode(cipher1.key.substring(0, 10)) === "aaaaaaaaaa";
+    const plaintext = "abcdefghij";
+    ans[2] = cipher1.decode(cipher1.encode(plaintext)) === plaintext;
+
+    const key = "abcdefghij";
+    const cipher2 = new Cipher(key);
+    ans[3] = cipher2.encode("aaaaaaaaaa") === "abcdefghij";
+    ans[4] = cipher2.decode(cipher2.encode("abcdefghij")) === "abcdefghij";
+    ans[5] =
+        new Cipher("iamapandabear").encode("iamapandabear") === "qayaeaagaciai";
+    ans[6] = cipher2.encode("zzzzzzzzzz") === "zabcdefghi";
+    ans[7] = cipher2.decode("zabcdefghi") === "zzzzzzzzzz";
+    ans[8] = new Cipher("abc").encode("iamapandabear") === "iboaqcnecbfcr";
+    ans[9] = new Cipher("abc").decode("iboaqcnecbfcr") === "iamapandabear";
+`;
+
+eval(input.value);
+
+for (let i = 0; i < ans.length; i++) {
+    output.value += `ans[${i}] = ${ans[i]}\n`;
+}
+
+submitBtn.addEventListener("click", () => {
+    ans = [];
+
+    try {
+        eval(input.value);
+        output.value = "";
+    } catch (error) {
+        output.value = error;
+        console.error(error);
+    }
+
+    for (let i = 0; i < ans.length; i++) {
+        output.value += `ans[${i}] = ${ans[i]}\n`;
+    }
+});
