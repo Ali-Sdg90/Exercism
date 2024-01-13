c24ef20e14f60bc683e9a6eea2fa1ac599653965
Ali Sadeghi <ali909392@gmail.com>
2024-01-13 21:53:46 +0330

    Simple Cipher-TS test cases works!!!

--- Simple Cipher-TS/TS/index.ts
+++ Simple Cipher-TS/TS/index.ts
@@ -1,5 +1,4 @@
 interface CipherInterface {
-    keyValue: string;
     encodeValue: string;
     decodeValue: string;
 }
@@ -9,24 +8,20 @@ export class SimpleCipher implements CipherInterface {
     encodeValue: string;
     decodeValue: string;
 
-    constructor(key = "") {
-        this.keyValue = key;
+    constructor(key?: string) {
+        this.keyValue = key || this.createRandomKey();
         this.encodeValue = "";
         this.decodeValue = "";
     }
 
     encode(input: string) {
-        if (!this.keyValue) {
-            this.noKeyGiven(input.length);
-        } else if (this.keyValue.length < input.length) {
-            this.loopGivenKey(input.length);
-        }
+        this.checkInput(input);
 
         this.encodeValue = "";
         for (let i = 0; i < input.length; i++) {
             let charCode =
                 input.charAt(i).charCodeAt(0) +
-                (this.key.charAt(i).charCodeAt(0) - 97);
+                (this.keyValue.charAt(i).charCodeAt(0) - 97);
 
             if (charCode > 122) {
                 charCode -= 26;
@@ -39,17 +34,13 @@ export class SimpleCipher implements CipherInterface {
     }
 
     decode(input: string) {
-        if (!this.keyValue) {
-            this.noKeyGiven(input.length);
-        } else if (this.keyValue.length < input.length) {
-            this.loopGivenKey(input.length);
-        }
+        this.checkInput(input);
 
         this.decodeValue = "";
         for (let i = 0; i < input.length; i++) {
             let charCode =
                 input.charAt(i).charCodeAt(0) -
-                (this.key.charAt(i).charCodeAt(0) - 97);
+                (this.keyValue.charAt(i).charCodeAt(0) - 97);
 
             if (charCode < 97) {
                 charCode += 26;
@@ -61,10 +52,31 @@ export class SimpleCipher implements CipherInterface {
         return this.decodeValue;
     }
 
-    noKeyGiven(inputLength: number) {
-        // console.log("noKeyGiven");
+    checkInput(input: string) {
+        if (!this.keyValue) {
+            this.noKeyGiven(input.length);
+        } else if (this.keyValue.length < input.length) {
+            this.loopGivenKey(input.length);
+        }
+    }
 
-        for (let i = 0; i < inputLength; i++) {
+    createRandomKey(): string {
+        let key = "";
+        for (let i = 0; i < 100; i++) {
+            key += this.randomLetter();
+        }
+        return key;
+    }
+
+    randomLetter(): string {
+        const i = Math.floor(Math.random() * 26);
+        return String.fromCharCode(97 + i);
+    }
+
+    noKeyGiven(inputLength: number) {
+        const minLength = Math.max(inputLength, 100);
+
+        while (this.keyValue.length < minLength) {
             this.keyValue += String.fromCharCode(
                 Math.trunc(Math.random() * 26) + 97
             );
@@ -72,85 +84,13 @@ export class SimpleCipher implements CipherInterface {
     }
 
     loopGivenKey(inputLength: number) {
-        // console.log("loopGivenKey");
-
-        const loopNumber = inputLength / this.keyValue.length;
-        for (let i = 0; i < loopNumber; i++) {
+        while (this.keyValue.length < inputLength) {
             this.keyValue += this.keyValue;
         }
+        this.keyValue = this.keyValue.substring(0, inputLength);
     }
 
     get key() {
         return this.keyValue;
     }
 }
-
-// Tests:
-
-// const cipher3 = new Cipher();
-// console.log(cipher3.encode("aaaaaaaaaa") === cipher3.key.substring(0, 10));
-// console.log(cipher3.decode(cipher3.key.substring(0, 10)) === "aaaaaaaaaa");
-// const plaintext2 = "abcdefghij";
-// console.log(cipher3.decode(cipher3.encode(plaintext2)) === plaintext2);
-
-// const key2 = "abcdefghij";
-// const cipher4 = new Cipher(key2);
-// console.log(cipher4.encode("aaaaaaaaaa") === "abcdefghij");
-// console.log(cipher4.decode("abcdefghij") === "aaaaaaaaaa");
-// console.log(cipher4.decode(cipher4.encode("abcdefghij")) === "abcdefghij");
-// console.log(
-//     new Cipher("iamapandabear").encode("iamapandabear") === "qayaeaagaciai"
-// );
-// console.log(cipher4.encode("zzzzzzzzzz") === "zabcdefghi");
-// console.log(cipher4.decode("zabcdefghi") === "zzzzzzzzzz");
-// console.log(new Cipher("abc").encode("iamapandabear") === "iboaqcnecbfcr");
-// console.log(new Cipher("abc").decode("iboaqcnecbfcr") === "iamapandabear");
-
-// // Showcase:
-
-// const input = document.getElementById("input-text") as HTMLInputElement;
-// const output = document.getElementById("output") as HTMLInputElement;
-// const submitBtn = document.getElementById("btn") as HTMLElement;
-
-// let test: string[] = [];
-
-// input.value = `
-//     const cipher1 = new Cipher();
-//     test[0] = cipher1.encode("aaaaaaaaaa") === cipher1.key.substring(0, 10);
-//     test[1] = cipher1.decode(cipher1.key.substring(0, 10)) === "aaaaaaaaaa";
-//     const plaintext = "abcdefghij";
-//     test[2] = cipher1.decode(cipher1.encode(plaintext)) === plaintext;
-
-//     const key = "abcdefghij";
-//     const cipher2 = new Cipher(key);
-//     test[3] = cipher2.encode("aaaaaaaaaa") === "abcdefghij";
-//     test[4] = cipher2.decode(cipher2.encode("abcdefghij")) === "abcdefghij";
-//     test[5] =
-//         new Cipher("iamapandabear").encode("iamapandabear") === "qayaeaagaciai";
-//     test[6] = cipher2.encode("zzzzzzzzzz") === "zabcdefghi";
-//     test[7] = cipher2.decode("zabcdefghi") === "zzzzzzzzzz";
-//     test[8] = new Cipher("abc").encode("iamapandabear") === "iboaqcnecbfcr";
-//     test[9] = new Cipher("abc").decode("iboaqcnecbfcr") === "iamapandabear";
-// `;
-
-// eval(input.value);
-
-// for (let i = 0; i < test.length; i++) {
-//     output.value += `test[${i}] = ${test[i]}\n`;
-// }
-
-// submitBtn.addEventListener("click", () => {
-//     test = [];
-
-//     try {
-//         eval(input.value);
-//         output.value = "";
-//     } catch (error: any) {
-//         output.value = error;
-//         console.error(error);
-//     }
-
-//     for (let i = 0; i < test.length; i++) {
-//         output.value += `test[${i}] = ${test[i]}\n`;
-//     }
-// });
